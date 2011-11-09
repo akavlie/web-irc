4e896d34ec90c2b5216f6be8e37b1e41fbae2927
Aaron Kavlie <akavlie@gmail.com>
2011-11-09 12:43:51 -0700

    Add code from HTML5 Boilerplate to prevent JS errors when console is unavailable.

--- js/util.js
+++ js/util.js
@@ -1,3 +1,7 @@
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
+
 // POLYFILLS
 // =========
 
